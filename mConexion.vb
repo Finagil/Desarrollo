aa835eca2e37e9da2c284fde5f6f88bf3a5d0362
ecacerest <ecacerest@finagil.com.mx>
2020-07-23 17:55:11 -0500

    Synk

--- mConexion.vb
+++ mConexion.vb
@@ -7,8 +7,8 @@ Module mConexion
     Public strConnPRO As String
 
     Public Sub CreaCadenaConexion(ByVal Usuario As String, ByVal Password As String)
-        strConn = "Server=SERVER-RAID2\DBRESPALDOS, 62887; DataBase=2019oct; User ID=User_PRO; pwd=User_PRO2015"
-        strConnPRO = "Server=SERVER-RAID2\DBRESPALDOS, 62887; DataBase=2019oct; User ID=User_PRO; pwd=User_PRO2015" 'guarda la mezcla del mes en produccion
+        strConn = "Server=SERVER-RAID2\DBRESPALDOS, 62887; DataBase=2020jun; User ID=User_PRO; pwd=User_PRO2015"
+        strConnPRO = "Server=SERVER-RAID2\DBRESPALDOS, 62887; DataBase=2020jun; User ID=User_PRO; pwd=User_PRO2015" 'guarda la mezcla del mes en produccion
     End Sub
 
 End Module
